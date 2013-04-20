4c5abc73403522e3c565aab66b1ff4c631214fb2
Krisztian Fekete <fkr972@gmail.com>
2013-04-20 17:08:32 +0200

    test: tempdir -> temp_dir.within_temp_dir

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 
 from setuptools import setup
 
-setup(name='csvtools',
-      version='0.1.0-dev',
-      description=u'Tools for transforming .csv files',
-      author=u'Krisztián Fekete',
-#       author_email='fkr972',
-#       url='http://maybe.later',
-      packages=['csvtools'],
-      # install_requires=[],
-      provides=['csvtools (0.1.0)']
-     )
+setup(
+    name='csvtools',
+    version='0.1.0-dev',
+    description=u'Tools for transforming .csv files',
+    author=u'Krisztián Fekete',
+    url='https://github.com/krisztianfekete/csvtools',
+    packages=['csvtools'],
+    install_requires=['temp_dir'],
+    provides=['csvtools (0.1.0)']
+    )
