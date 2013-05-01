f026b72b4c44c97d7d3e952f640d5cde9d89f7c9
Krisztian Fekete <fkr972@gmail.com>
2013-05-01 21:54:38 +0200

    console scripts

--- setup.py
+++ setup.py
@@ -3,13 +3,32 @@
 
 from setuptools import setup
 
+VERSION = '0.20130501.0'
+VERSION_SUFFIX = '-dev'
+
 setup(
     name='csvtools',
-    version='0.1.0-dev',
+    version='{version}{version_suffix}'.format(
+        version=VERSION, version_suffix=VERSION_SUFFIX),
+
     description=u'Tools for transforming .csv files',
     author=u'Krisztián Fekete',
     url='https://github.com/krisztianfekete/csvtools',
+
     packages=['csvtools'],
-    install_requires=['temp_dir'],
-    provides=['csvtools (0.1.0)']
+
+    install_requires=['temp_dir'],  # not really - only for tests
+
+    provides=['csvtools ({version})'.format(version=VERSION)],
+
+    entry_points={
+        'console_scripts': [
+            'csv_select = csvtools.select:main',
+            'csv_split = csvtools.split:main',
+            'csv_zip = csvtools.zip:main',
+            'csv_unzip = csvtools.unzip:main',
+            'csv_rmfields = csvtools.rmfields:main',
+            'csv_extract_map = csvtools.extract_map:main'
+        ],
+    }
     )
